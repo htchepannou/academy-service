bbc1e87d66855822ce28fffe9921893416a2659c
htchepannou <htchepannou@expedia.com>
2017-09-06 21:58:27 -0400

    update description

--- src/main/resources/db/migration/V1_1__course-how_to_write_README.sql
+++ src/main/resources/db/migration/V1_1__course-how_to_write_README.sql
@@ -13,7 +13,7 @@ INSERT INTO T_VIDEO(id, type_fk, video_id, duration_second) VALUES(10114, 1, 'rr
 
 
 INSERT INTO T_COURSE(id, status_fk, level_fk, title, summary, description, language, published_datetime, updated_datetime)
-  VALUES(100, 2, 1, 'How to write README', 'Learn the importance of well documented code and see how to craft meaningful READMEs.', null, 'en', now(), now());
+  VALUES(100, 2, 1, 'How to write README', 'Learn the importance of well documented code and see how to craft meaningful READMEs.', 'Documentation is an important part of the development process. Learn to write READMEs using Markdown so your code can be used by other humans!', 'en', now(), now());
 
 INSERT INTO T_LESSON(id, course_fk, title, rank) VALUE(101, 100, 'Writing READMEs', 1);
 
